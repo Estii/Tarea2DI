16591c7ef7df09e9dc9f3d5eb1d32a3093ccf790
Francisco Riveros <francisco.riverosc@sansano.usm.cl>
2020-11-28 18:53:17 -0300

    Update DataNode.go

--- DataNode.go
+++ DataNode.go
@@ -107,8 +107,12 @@ func (s *Server) EnviarLibro(ctx context.Context, message *cliente.MessageClient
 		cantidad := message.CantidadChunks
 		cantidad_uniforme := cantidad/3
 		cantidad_resto := cantidad%3
-		message := nodos.MessageNode{ Cantidad1:cantidad_uniforme + cantidad_resto, Cantidad2:cantidad_uniforme,Cantidad3:cantidad_uniforme,NombreLibro:nombre_libro,ID: IDNODE}
-		Propuesta(&message)
+		
+		if(message.Tipo == 2){
+			message := nodos.MessageNode{ Cantidad1:cantidad_uniforme + cantidad_resto, Cantidad2:cantidad_uniforme,Cantidad3:cantidad_uniforme,NombreLibro:nombre_libro,ID: IDNODE}
+			Propuesta(&message)
+		}
+		
 		nombre_libro = " "
 		listachunks = listachunks[:0]
 		return &cliente.ResponseCliente{},nil
