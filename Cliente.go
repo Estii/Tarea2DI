23f2ff695287264a6ad44f9a8c9cd7dd41e67cd0
Francisco Riveros <francisco.riverosc@sansano.usm.cl>
2020-11-28 18:18:25 -0300

    Update Cliente.go

--- Cliente.go
+++ Cliente.go
@@ -101,7 +101,7 @@ func main() {
 			log.Fatalf("Error al conectar con el servidor: %s", err)
 		}else{
 			c := chat.NewChatServiceClient(conn)		
-			response,err := c.CheckEstado(context.Background(),&chat.EstadoE{Estado:1})
+			response,_ := c.CheckEstado(context.Background(),&chat.EstadoE{Estado:1})
 			if response.Estado == 1 {
 				flag = false
 			}  
