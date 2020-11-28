c8a7b7aecf62e64fc3f90d4800323e7a4ea36f63
Francisco Riveros <francisco.riverosc@sansano.usm.cl>
2020-11-28 04:43:21 -0300

    Update NameNode.go

--- NameNode.go
+++ NameNode.go
@@ -101,22 +101,22 @@ func (s *Server) Propuesta(ctx context.Context, message *nodos.MessageNode) (*no
 	if(flag==0){	
 		fmt.Println("Propuesta aceptada")
 
-		for k:=1;i<message.Cantidad1;k++{
-			b := []byte(message.NombreLibro+"_"+strconv.FormatInt(k,10)+" dist109\n")
+		for k:=1;k<message.Cantidad1;k++{
+			b := []byte(message.NombreLibro+"_"+strconv.Itoa(k)+" dist109\n")
 			err := ioutil.WriteFile("Log/log.txt", b, 0644)
 			if err != nil {
 				log.Fatal(err)
 			}  
 		}
-		for k:=1;i<message.Cantidad2;k++{
-			b := []byte(message.NombreLibro+"_"+strconv.FormatInt(k,10)+" dist110\n")
+		for k:=1;k<message.Cantidad2;k++{
+			b := []byte(message.NombreLibro+"_"+strconv.Itoa(k)+" dist110\n")
 			err := ioutil.WriteFile("Log/log.txt", b, 0644)
 			if err != nil {
 				log.Fatal(err)
 			}  
 		}		
-		for k:=1;i<message.Cantidad3;k++{
-			b := []byte(message.NombreLibro+"_"+strconv.FormatInt(k,10)+" dist111\n")
+		for k:=1;k<message.Cantidad3;k++{
+			b := []byte(message.NombreLibro+"_"+strconv.Itoa(k)+" dist111\n")
 			err := ioutil.WriteFile("Log/log.txt", b, 0644)
 			if err != nil {
 				log.Fatal(err)
